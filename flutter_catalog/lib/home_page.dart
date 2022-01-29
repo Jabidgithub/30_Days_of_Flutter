66b919ebfb56dbf9d75381c52a38ab64a0189b47
Jabidgithub <jabid9931@gmail.com>
2022-01-29 16:16:43 +0600

    Day 2 Completed

---
+++ flutter_catalog/lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Jabid Hasan";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+          body: Center(
+            child: Container(
+              child: Text("Welcome to $days days of Fultter by ${name}"),
+            ),
+          ),
+          drawer: Drawer(),
+        );
+  }
+}
